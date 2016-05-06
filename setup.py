87c64d5f5eaca7aa9d9fd91ac84da6de70dfd009
Jeandre Le Roux <theblazehen@theblazehen.com>
2016-05-06 08:20:44 +0200

    Fix package error for real

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@
 
 from setuptools import setup
 
-with open("README.md") as f:
-    readme = f.read()
-
 setup(name="callbackToReturnVal",
     version="0.1.1",
     description="Wrapper function that returns the value that is sent to a callback value as the value that the wrapper gives",
-    long_description=readme,
     licence='MIT',
     author="Jeandre Le Roux",
     author_email="theblazehen@theblazehen.com",
